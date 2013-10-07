fd3827e37f3d07042df497475dd4c831fc35a6d6
Srikanth <srikanth@thoughworks.com>
2013-10-07 13:13:38 +0530

    initial application setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bc140::Application.config.secret_key_base = '06b9bbb5e7256c401d0e7762cce5ea9df61ef8f7d106b1a5dec4047d407ef9aa8a33ac5105e86d70b58ff882a0e4bffb8a64dae00ec22e4f2e74629807b3ce5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bc140::Application.config.session_store :cookie_store, key: '_bc140_session'
