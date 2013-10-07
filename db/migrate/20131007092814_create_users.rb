d5b8caaa7d7d453c1f804be7c348c00beea71f12
Srikanth <srikanth@thoughworks.com>
2013-10-07 15:36:25 +0530

    Added create users migration

---
+++ db/migrate/20131007092814_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+        t.string :handle, :null => false
+        t.string :name , :null => false
+        t.string :location
+        t.string :website
+        t.string :bio
+        t.string :photo
+        t.integer :lock_version, :default => 0
+        t.timestamps
+    end
+  end
+end
